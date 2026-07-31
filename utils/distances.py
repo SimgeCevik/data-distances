e374c6baa2cdf37603fbdc52917074d6a0245a0b
Simge Çevik <144052451+SimgeCevik@users.noreply.github.com>
2026-07-31 16:09:14 +0300

    fix(utils): euclidean distance with sqrt

--- utils/distances.py
+++ utils/distances.py
@@ -1,3 +1,5 @@
+import math
+
 def manhattan(a,b):
     return sum(abs(x  - y) for x, y in zip(a, b))
 
@@ -5,5 +7,5 @@ def euclidean(a, b):
     d_x = b[0] - a[0]
     d_y = b[1] - a[1]
 
-    distance = d_x**2 + d_y**2
+    distance = math.sqrt(d_x**2 + d_y**2)
     return distance
